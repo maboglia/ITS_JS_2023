c87f96029c9e4246cd9945ca34a6ac3142be0fa9
Mauro Bogliaccino <m.bogliaccino@studenti.local>
2023-09-18 17:56:12 +0200

    agg

--- codice/Lez07/script.js
+++ codice/Lez07/script.js
@@ -23,46 +23,125 @@
 
 //arrow function
 
-const test =  params => console.log('test');
+// const test =  params => console.log('test');
 
-const note = ['do', 're', 'mi', 'fa', 'do#'] 
+// const note = ['do', 're', 'mi', 'fa', 'do#'] 
 
-// for (const nota of note) {
-//     console.log(nota.toUpperCase());
+// // for (const nota of note) {
+// //     console.log(nota.toUpperCase());
+// // }
+
+// const noted = note
+//     .filter(n => n.startsWith("d"));
+
+//     console.log(noted);
+
+
+// //spread operator
+
+// let a = ['a', 'b', 'c']
+
+// let b = [...a, 'd','e']
+
+// console.log(b);
+
+// //rest parameters
+// function avg(...params) {
+//     let totale = 0;
+//     for (let index = 0; index < params.length; index++) {
+//         totale += params[index];
+        
+//     }
+//     return totale / params.length;
+
 // }
 
-const noted = note
-    .filter(n => n.startsWith("d"));
-
-    console.log(noted);
 
 
-//spread operator
+// console.log(avg(25,26,27,28,29,30));
+// console.log(avg(28,29,30));
 
-let a = ['a', 'b', 'c']
-
-let b = [...a, 'd','e']
-
-console.log(b);
-
-//rest parameters
-function avg(...params) {
-    let totale = 0;
-    for (let index = 0; index < params.length; index++) {
-        totale += params[index];
-        
-    }
-    return totale / params.length;
-
-}
+// const totale = [25,26,27,28,29,30].reduce((a, b)=>a+b,0)/6;
+// console.log(totale);
 
 
+//destructuring array
 
-console.log(avg(25,26,27,28,29,30));
-console.log(avg(28,29,30));
+// const presenze = ['Biagina Antonella','Alberti',22,84.62,2,4,4,4,4,4];
 
-const totale = [25,26,27,28,29,30].reduce((a, b)=>a+b,0)/6;
-console.log(totale);
+// const [nome, cognome,,, ...ore] = presenze;
+// console.log(cognome);
+// console.log(ore.reduce((a,c)=>a+c, 0));
+
+// //destrutturare oggetti
+
+// const libro = {
+//     titolo: 'Io, robot',
+//     autore: 'Asimov',
+//     lingue: {
+//         orig: 'russo',
+//         trad: 'inglese'
+//     }
+// }
+
+// const { autore, lingue: {orig}, tipo = 'romanzo' } = libro;
+
+// console.log(orig);
+// console.log(tipo);
+
+//destrutturare oggetti / funzioni
+
+// function print( {nome, cognome} ) {
+//     console.log(`Benvenuto ${nome} ${cognome}`);
+// }
+
+// const persona = {
+//     nome: 'mauro',
+//     cognome: 'bogliaccino',
+//     corsi: ['java', 'js', 'php']
+// }
+
+// print(persona)
+
+//SET collezione che non ammette duplicati
+
+// let moto = new Set();
+
+// moto.add('Yamaha');
+// moto.add('Honda');
+// moto.add('BMW');
+// moto.add('Yamaha');
+// moto.add('Suzuki');
+// moto.add('Ducati');
+// moto.add('BMW');
+
+// console.log(moto);
+
+//MAP
+
+// const regioni = new Map();
+
+// regioni.set("piemonte", 'torino');
+// regioni.set("lombardia", 'milano');
+// regioni.set("lazio", 'roma');
+
+// for (const [regione, citta] of regioni) {
+//     console.log(`La città capoluogo della regione ${regione} è ${citta}`);
+// }
+
+//.map()
+
+let prodotti = [
+    {prodotto: 'telefono apple', qty: 100},
+    {prodotto: 'telefono samsung', qty: 50},
+    {prodotto: 'telefono motorola', qty: 80},
+    {prodotto: 'telefono huawey', qty: 70},
+]
+
+let telefoni = prodotti.map(p => p.prodotto)
+
+console.log(telefoni);
+
 
 
 
